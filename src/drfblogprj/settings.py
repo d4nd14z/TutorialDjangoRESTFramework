1a9a0475c01afb33683834cb18df966c511080cf
d4nd14z <dandiazfo@gmail.com>
2023-03-26 19:56:52 -0500

    Endpoints: Login y Refresh Token para autenticacion de usuarios

--- src/drfblogprj/settings.py
+++ src/drfblogprj/settings.py
@@ -114,6 +114,12 @@ USE_I18N = True
 
 USE_TZ = True
 
+# Configuracion de Autenticacion con JWT
+REST_FRAMEWORK = { 
+    'DEFAULT_AUTHENTICATION_CLASSES': (        
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )    
+}
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
