551272612d2d1e6b24e39ec3196436705fbda764
d4nd14z <dandiazfo@gmail.com>
2023-03-26 19:26:57 -0500

    Endpoint: Registro de Usuarios

--- src/users/api/views.py
+++ src/users/api/views.py
@@ -3,9 +3,13 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from users.models import User
+from users.api.serializers import UserRegisterSerializer
 
 class RegisterView(APIView):
 
     def post(self, request):
-        print('Registrando usuarios...') 
-        return Response(status=status.HTTP_200_OK, data='Todo OK !!!')
+        serializer = UserRegisterSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
