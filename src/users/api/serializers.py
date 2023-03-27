9aaf5f44e0e304ac6b8c1bc6ab7defdaae7aed35
d4nd14z <dandiazfo@gmail.com>
2023-03-26 19:32:16 -0500

    Endpoint: Registro de Usuarios / Encriptando la contrasena en el registro de usuarios

--- src/users/api/serializers.py
+++ src/users/api/serializers.py
@@ -5,3 +5,12 @@ class UserRegisterSerializer(serializers.ModelSerializer):
     class Meta: 
         model = User
         fields = ['id', 'email', 'username', 'password']
+
+    #Metodo para realizar la encripcion de la contrasena del usuario
+    def create(self, validated_data): 
+        password = validated_data.pop('password', None)
+        instance = self.Meta.model(**validated_data)
+        if password is not None: 
+            instance.set_password(password)
+        instance.save()
+        return instance
