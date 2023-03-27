735454b4d35f89e0a745df670135c697f88a16f8
d4nd14z <dandiazfo@gmail.com>
2023-03-26 21:06:21 -0500

    Endpoints Category: Creacion de endpoints para el CRUD de Categories

--- src/drfblogprj/urls.py
+++ src/drfblogprj/urls.py
@@ -20,6 +20,8 @@ from django.urls import include, path
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
 
+from categories.api.router import router_categories
+
 schema_view = get_schema_view(
    openapi.Info(
       title="Django REST Framework / Blog API Documentation",
@@ -39,4 +41,5 @@ urlpatterns = [
     path('docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redocs/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('api/', include('users.api.router')),
+    path('api/', include(router_categories.urls)),
 ]
