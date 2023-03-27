861200ed94695e5729a6b2da6293ce8dd5696674
d4nd14z <dandiazfo@gmail.com>
2023-03-26 19:04:57 -0500

    Generando la documentacion de la API

--- src/drfblogprj/settings.py
+++ src/drfblogprj/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework', # Esta es la instalacion de Django REST Framework
+    'drf_yasg',       # Este es el sistema de documentacion de la API
     'users',
 ]
 
@@ -118,6 +119,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATIC_ROOT = './static/' # Archivos estaticos de la Documentacion del Proyecto
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
--- src/drfblogprj/urls.py
+++ src/drfblogprj/urls.py
@@ -16,6 +16,26 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+#Importaciones para el Sistema de documentacion de la API
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Django REST Framework / Blog API Documentation",
+      default_version='v1',
+      description="Documentacion de la API del Blog",
+      terms_of_service="",
+      contact=openapi.Contact(email="admin@localhost.com"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   #permission_classes=[permissions.AllowAny],
+)
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redocs/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
