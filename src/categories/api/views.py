dbbc6cf74ab42febb4b08fc02a47665e943959c6
d4nd14z <dandiazfo@gmail.com>
2023-03-26 21:45:07 -0500

    Se cambia el parametro de busqueda de los EndPoints de Category, se cambia la busqueda del ID por un SLUG

--- src/categories/api/views.py
+++ src/categories/api/views.py
@@ -8,3 +8,4 @@ class CategoryApiViewSet(ModelViewSet):
     permission_classes = [IsAdminOrReadOnly]
     serializer_class = CategorySerializer
     queryset = Category.objects.all()
+    lookup_field = 'slug'  #Cambiar el ID por la propiedad 'slug'. Cuando se envien parametros a la API no se envia el ID de la categoria, debe enviarse el SLUG.
